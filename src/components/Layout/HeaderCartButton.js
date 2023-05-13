6686dd79f48f6db37f5ebb9e7310798a32474810
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2023-05-13 14:41:08 +0300

    Using the Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import clases from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  /*reduces - method which allows us to transform an array of data into a single value*/
+  /*The second argument is a starting value */
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={clases.button} onClick={props.onClick}>
       {/* icon cart */}
@@ -15,7 +24,7 @@ const HeaderCartButton = (props) => {
       <span>Your Cart</span>
 
       {/* the total amount of items in the cart */}
-      <span className={clases.badge}>3</span>
+      <span className={clases.badge}>{numberOfCartItems}</span>
     </button>
   );
 };

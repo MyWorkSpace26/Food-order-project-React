a94c8321ea03bc96e35cdafac62fb5848eec4068
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2023-05-13 11:11:45 +0300

    Adding a Modal via a React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,27 @@
-import clases from "./CartItem.module.css";
+import Modal from "../UI/Modal";
+
+import clases from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartitems = (
-    <ul className={calses["cart-item"]}>
+    <ul className={clases["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: "2", price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartitems}
       <div className={clases.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={calses.actions}>
+      <div className={clases.actions}>
         <button className={clases["button--alt"]}>Close</button>
         <button className={clases["button"]}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 

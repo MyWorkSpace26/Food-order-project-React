7575f5016f87ba2591efd897f424a2217f0956ec
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2023-06-13 13:57:58 +0300

    Submitting & Sending Cart Data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -51,6 +51,16 @@ const Cart = (props) => {
     </div>
   );
 
+  const submitOrderHandler = (userData) => {
+    fetch("https://react-http-a5d84-default-rtdb.firebaseio.com/orders.json", {
+      method: "POST",
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    });
+  };
+
   return (
     <Modal onClose={props.onStop}>
       {cartitems}
@@ -58,7 +68,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onStop} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onStop} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
